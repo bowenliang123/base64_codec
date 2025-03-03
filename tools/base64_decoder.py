d58660ce0c802ca378b5d870f1f60d323e4d6338
Bowen Liang <liangbowen@gf.com.cn>
2025-03-03 21:46:31 +0800

    yield empty string when empty input

--- tools/base64_decoder.py
+++ tools/base64_decoder.py
@@ -10,7 +10,8 @@ class Base64TextDecodeTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         encoded_text: str = tool_parameters.get("encoded_text")
         if not encoded_text:
-            raise ValueError("Invalid input encoded_text")
+            yield self.create_text_message("")
+            return
 
         decoded_str: str = base64.b64decode(encoded_text.encode("utf-8")).decode("utf-8")
         yield self.create_text_message(decoded_str)
--- tools/base64_encoder.py
+++ tools/base64_encoder.py
@@ -10,7 +10,8 @@ class Base64TextEncodeTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         plain_text: str = tool_parameters.get("plain_text")
         if not plain_text:
-            raise ValueError("Invalid input plain_text")
+            yield self.create_text_message("")
+            return
 
         encoded_str: str = base64.b64encode(plain_text.encode("utf-8")).decode("utf-8")
         yield self.create_text_message(encoded_str)
