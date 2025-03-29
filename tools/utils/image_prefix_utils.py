933e80878ce75a00a7942714fc94678c1c38f674
Bowen Liang <liangbowen@gf.com.cn>
2025-03-30 02:02:26 +0800

    nit

--- tools/utils/image_prefix_utils.py
+++ tools/utils/image_prefix_utils.py
@@ -1,6 +1,7 @@
 MIME_TYPE_TO_PREFIX = {
     "image/png": "data:image/png;base64",
     "image/jpeg": "data:image/jpeg;base64",
+    "image/gif": "data:image/gif;base64",
     "image/webp": "data:image/webp;base64",
     "image/svg+xml": "data:image/svg+xml;base64"
 }
