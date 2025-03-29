adf489ff29f7512da7b2e45a1ec7537631bc3477
Bowen Liang <liangbowen@gf.com.cn>
2025-03-30 00:30:58 +0800

    update _validate_credentials

--- provider/base64_codec.py
+++ provider/base64_codec.py
@@ -3,6 +3,10 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
+from tools.base64_decoder import Base64TextDecodeTool
+from tools.base64_encoder import Base64TextEncodeTool
+from tools.base64_image_decoder import Base64ImageDecodeTool
+
 
 class Base64CodecProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
@@ -10,5 +14,8 @@ class Base64CodecProvider(ToolProvider):
             """
             IMPLEMENT YOUR VALIDATION HERE
             """
+            Base64TextEncodeTool.from_credentials({})
+            Base64TextDecodeTool.from_credentials({})
+            Base64ImageDecodeTool.from_credentials({})
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
