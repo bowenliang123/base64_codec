aa53993ee5e7332af9c2fd14c183de3549832127
Bowen Liang <liangbowen@gf.com.cn>
2025-04-17 23:02:23 +0800

    nit

--- provider/base64_codec.py
+++ provider/base64_codec.py
@@ -5,6 +5,7 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
 from tools.base64_decoder.base64_decoder import Base64TextDecodeTool
 from tools.base64_encoder.base64_encoder import Base64TextEncodeTool
+from tools.base64_file_encoder.base64_file_encoder import Base64FileEncodeTool
 from tools.base64_image_decoder.base64_image_decoder import Base64ImageDecodeTool
 from tools.base64_image_encoder.base64_image_encoder import Base64ImageEncodeTool
 
@@ -19,5 +20,6 @@ class Base64CodecProvider(ToolProvider):
             Base64TextDecodeTool.from_credentials({})
             Base64ImageEncodeTool.from_credentials({})
             Base64ImageDecodeTool.from_credentials({})
+            Base64FileEncodeTool.from_credentials({})
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
