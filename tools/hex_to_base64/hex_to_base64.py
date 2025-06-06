014cf75952cf62d1939a268a029ddcdc63631f8a
Bowen Liang <liangbowen@gf.com.cn>
2025-06-06 20:28:13 +0800

    fix hex_to_base64 tool

--- tools/hex_to_base64/hex_to_base64.py
+++ tools/hex_to_base64/hex_to_base64.py
@@ -4,16 +4,23 @@ from typing import Any
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
-from tools.utils.base64_utils import encode_to_base64, encode_bytes_to_base64
+from tools.utils.base64_utils import encode_bytes_to_base64
 
 
 class HexToBase64Tool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        hex_text: str = tool_parameters.get("hex_text")
+        hex_text: str = tool_parameters.get("hex_text", "").strip()
+
+        # remove possible "0x" prefix
+        hex_text = hex_text.removeprefix("0x")
+
+        if len(hex_text) % 2 != 0:
+            raise ValueError(f"Invalid hex text, exception: the length of hex text must be an even number.")
+
         try:
-            hex_bytes=bytes.fromhex(hex_text)
-        except Exception:
-            raise ValueError("Failed to decode hex text")
+            hex_bytes = bytes.fromhex(hex_text)
+        except ValueError as e:
+            raise ValueError(f"Invalid hex text, exception: {e}")
 
         urlsafe_enabled = bool("true" == tool_parameters.get("urlsafe_enabled", "").lower())
         try:
